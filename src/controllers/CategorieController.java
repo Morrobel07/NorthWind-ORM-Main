d2b38047efe216d94da97240ded0985d61844fea
freshdl <freshdl@gmail.com>
2025-09-10 23:56:52 -0400

    Revisar

---
+++ src/controllers/CategorieController.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import comons.IFile;
+import models.Categorie;
+import repository.CategorieRepository;
+
+import java.util.List;
+
+public class CategorieController {
+      private IFile<Categorie> repository ;
+
+      public CategorieController(IFile<Categorie> repository) {
+          this.repository = repository;
+      }
+
+      public List<Categorie> getAllCategories() {
+          try{
+              return repository.GetAll();
+          }catch (Exception e){
+              System.out.println( "Error al obtener todas las categorias "+ e.getMessage());
+              return null;
+          }
+      }
+
+      public Categorie getCategorieById(String id) {
+          try {
+              return repository.GetOne(id);
+          } catch (Exception e) {
+              System.out.println( "Error al obtener todas las categoria "+ e.getMessage());
+              return null;
+          }
+      }
+
+      public void addCategorie(Categorie categorie) {
+          try {
+              List<Categorie> categories =  getAllCategories();
+              categories.add(categorie);
+              repository.Save(categories);
+              System.out.println("Categoria agregada correctamente");
+           } catch (Exception e) {
+              System.out.println( "Error al agregar categoria"+ e.getMessage());
+          }
+
+      }
+
+      public void updateCategorie(Categorie categorie) {
+        try {
+            repository.Update(categorie);
+            System.out.println("Categoria actualizada correctamente");
+        }catch (Exception e) {
+            System.out.println( "Error al actualizar categoria"+ e.getMessage());
+        }
+      }
+
+      public void deleteCategorie(String id) {
+          try {
+              repository.Delete(id);
+          }catch (Exception e) {
+              System.out.println( "Error al eliminar Categoria"+ e.getMessage());
+          }
+      }
+
+
+}
---
+++ src/controllers/SupplierController.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import comons.IFile;
+import models.Employee;
+import repository.EmployeeRepository;
+
+import java.util.List;
+
+public class EmployeeController {
+      private IFile<Employee> repository ;
+
+      public EmployeeController(IFile<Employee> repository) {
+          this.repository = repository;
+      }
+
+      public List<Employee> getAllEmployees() {
+          try{
+              return repository.GetAll();
+          }catch (Exception e){
+              System.out.println( "Error al obtener todos los empleados "+ e.getMessage());
+              return null;
+          }
+      }
+
+      public Employee getEmployeeById(String id) {
+          try {
+              return repository.GetOne(id);
+          } catch (Exception e) {
+              System.out.println( "Error al obtener todos los empleados "+ e.getMessage());
+              return null;
+          }
+      }
+
+      public void addEmployee(Employee employee) {
+          try {
+              List<Employee> employees =  getAllEmployees();
+              employees.add(employee);
+              repository.Save(employees);
+              System.out.println("Empleado agregado correctamente");
+           } catch (Exception e) {
+              System.out.println( "Error al agregar empleado"+ e.getMessage());
+          }
+
+      }
+
+      public void updateEmployee(Employee employee) {
+        try {
+            repository.Update(employee);
+            System.out.println("Empleado actualizado correctamente");
+        }catch (Exception e) {
+            System.out.println( "Error al actualizar empleado"+ e.getMessage());
+        }
+      }
+
+      public void deleteEmployee(String id) {
+          try {
+              repository.Delete(id);
+          }catch (Exception e) {
+              System.out.println( "Error al eliminar empleado"+ e.getMessage());
+          }
+      }
+
+
+}
